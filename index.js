7699cd864fe6d7849c41a8a4c16c52d2c119ef0a
Lars Lien <lars@familienlien.com>
2020-07-05 10:57:09 +0200

    Added error message, when client Token is not set

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
+if (config.get("authService.clientToken") == "CLIENT_AUTH_STRING")
+  throw new Error("CLIENT_AUTH_STRING is not set");
+
 require("./frontend/index")(app);
 require("./backend/index")(app);
 
